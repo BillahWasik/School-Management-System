188f482f4ddb9cf4ffc5f85048b111279ae99074
Wasik <Wasik@DESKTOP-P0PU1B1>
2022-06-01 02:37:17 +0600

    More Models added

---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace School_Management_System.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string Class { get; set; }
+        public int roll { get; set; }
+        public string section { get; set; }
+    }
+}
---
+++ Models/Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace School_Management_System.Models
+{
+    public class Teacher
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int salary { get; set; }
+        public string designation { get; set; }
+    }
+}
