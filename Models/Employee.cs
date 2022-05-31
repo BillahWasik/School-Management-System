46b3e385c9d58f6b4d1161844442ff3982b1b49e
Wasik <Wasik@DESKTOP-P0PU1B1>
2022-06-01 02:17:22 +0600

    Employee Model added

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace School_Management_System.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public int salary { get; set; }
+
+        public string role { get; set; }
+    }
+}
