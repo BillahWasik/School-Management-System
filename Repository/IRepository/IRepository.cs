75e398185ee112eeedf6798d1c72708f5aa487df
Wasik <Wasik@DESKTOP-P0PU1B1>
2022-06-04 00:42:10 +0600

    New updates

---
+++ Repository/IRepository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace School_Management_System.Irepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+
+        void Add(T item);
+
+        void Remove(T item);
+    }
+}
