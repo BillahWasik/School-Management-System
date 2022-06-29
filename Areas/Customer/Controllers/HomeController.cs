b7b0b372fb89667988ba65d35cfe03ca5ae53b42
Wasik <Wasik@DESKTOP-P0PU1B1>
2022-06-30 02:14:29 +0600

    New Changes

--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 namespace School_Management_System.Controllers
 {
+    
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- Program.cs
+++ Program.cs
@@ -34,6 +34,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{Area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
